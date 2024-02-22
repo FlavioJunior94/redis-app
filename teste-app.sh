71ee9f06085b33cda261f51ff5081f79bb5ab990
Flavio <fjunior94@yahoo.com.br>
2024-02-21 21:14:15 -0300

    ajuste script de teste

---
+++ teste-app.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+RESULT="'wget -qO- http://localhost:8001"
+wget -q localhost:8081
+if [$? -eq 0]
+then
+    echo 'ok -servico no ar!'
+
+elif [[ $RESULT== *"Number"* ]]
+then
+    echo 'ok - number of visits'
+    echo $RESULT
+else
+    echo 'not ok - number of visits'
+    exit 1
+fi
